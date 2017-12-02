8e631a8f8460c192eff721efb20869baef55d698
chaos234 <info@deltanet-production.de>
2017-12-02 02:17:39 +0100

    Initial Commit

---
+++ CommunityCenterOverhaul/CommunityCenterBundleOverhaul.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewConfigFramework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using StardewValley.Menus;
+
+namespace CommunityCenterBundleOverhaul
+{
+    /// <summary>The mod entry point.</summary>
+    public class CommunityCenterBundleOverhaul : Mod
+    {
+
+        // Initialising Variables
+        internal static IModSettingsFramework Settings;
+        internal ModOptionSelection dropDown;
+        public String lang;
+        public ITranslationHelper i18n;
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            bool isLoaded = this.Helper.ModRegistry.IsLoaded("Juice805.StardewConfigMenu");
+            lang = helper.Translation.Locale;
+            i18n = helper.Translation;
+            
+            if (!isLoaded)
+            {
+                this.Monitor.Log("Initialisation failed because StardewConfigMenu seems not to be correctly installed. This Mod will now do nothing.", LogLevel.Error);
+            }
+            else
+            {
+                this.Monitor.Log("Initialisation finished. Bundels are now out of control :D", LogLevel.Info);
+                
+                Settings = IModSettingsFramework.Instance;
+                var options = ModOptions.LoadUserSettings(this);
+                Settings.AddModOptions(options);
+
+                String original = "";
+                String original_desc = "";
+                String v02 = "";
+                String v02_desc = "";
+                String v02a = "";
+                String v02a_desc = "";
+                String v02b = "";
+                String v02b_desc = "";
+                String v02c = "";
+                String v02c_desc = "";
+                String okButton = "";
+
+                var list = new ModSelectionOptionChoices();
+                list.Add("default", getTranslation(helper, "default", original));
+                list.Add("v02", getTranslation(helper, "v02", v02));
+                list.Add("v02a", getTranslation(helper, "v02a", v02a));
+                list.Add("v02b", getTranslation(helper, "v02b", v02b));
+                list.Add("v02c", getTranslation(helper, "v02c", v02c));
+
+                dropDown = options.GetOptionWithIdentifier<ModOptionSelection>("bundle") ?? new ModOptionSelection("bundle", "Bundels", list, 0, true);
+                options.AddModOption(dropDown);
+
+                dropDown.hoverTextDictionary = new Dictionary<string, string>
+                {
+                    {"default", getTranslation(helper, "default.desc", original_desc)},
+                    {"v02", getTranslation(helper, "v02.desc", v02_desc)},
+                    {"v02a", getTranslation(helper, "v02a.desc", v02a_desc)},
+                    {"v02b", getTranslation(helper, "v02b.desc", v02b_desc)},
+                    {"v02c", getTranslation(helper, "v02c.desc", v02c_desc)},
+                };                            
+
+                var saveButton = new ModOptionTrigger("okButton", getTranslation(helper, "okButton", okButton), OptionActionType.OK);
+                options.AddModOption(saveButton);
+
+                saveButton.ActionTriggered += (id) => {
+                    this.Monitor.Log("[CCBO] Changing Bundle ...");
+
+                    options.SaveUserSettings();
+
+                    this.Monitor.Log(lang);
+
+                    invalidateCache(this.Helper);
+                    this.Helper.Content.AssetEditors.Add(new CCBOBundlesEditor(this, this.Helper, dropDown));
+                    Game1.addHUDMessage(new HUDMessage("Changed Community Center Bundle to: " + dropDown.Selection, 3) { noIcon = true, timeLeft = HUDMessage.defaultTime });
+                    this.Monitor.Log("[CCBO] Bundle changed successfully. If smth. is missing, you must restart your game.");
+                };
+                SaveEvents.AfterLoad += SaveEvents_AfterLoad;
+            }
+            
+        }
+
+        private void invalidateCache(IModHelper helper)
+        {
+            string bundleXnb = "Data\\Bundle.xnb";
+            string JunimoNoteXnb = "LooseSprites\\JunimoNote.xnb";
+
+            this.Monitor.Log(bundleXnb + " " + JunimoNoteXnb);
+
+            helper.Content.InvalidateCache(bundleXnb);
+            helper.Content.InvalidateCache(JunimoNoteXnb);
+        }
+
+        /*********
+        ** Private methods
+        *********/
+        private String getTranslation(IModHelper helper, String identifier, String var1)
+        {
+            var1 = helper.Translation.Get(identifier);
+            return var1;
+        }      
+
+        private void SaveEvents_AfterLoad(object sender, EventArgs e)
+        {
+            if (Context.IsWorldReady)
+            {
+                this.Helper.Content.AssetEditors.Add(new CCBOJunimoEditor(this, this.Helper, dropDown));
+                this.Helper.Content.AssetEditors.Add(new CCBOBundlesEditor(this, this.Helper, dropDown));
+            }
+        }
+    }
+}
---
+++ CommunityCenterOverhaul/ParsBundles.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+
+namespace CommunityCenterBundleOverhaul
+{
+    using Newtonsoft.Json;
+
+    public partial class ParsBundles
+    {
+        [JsonProperty("Content")]
+        public Content[] Content { get; set; }
+
+        [JsonProperty("ID")]
+        public int ID { get; set; }
+
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+    }
+
+    public partial class CCBOConfigParser
+    {
+        [JsonProperty("List")]
+        public List[] List { get; set; }
+
+        [JsonProperty("modManifest")]
+        public ModManifest ModManifest { get; set; }
+
+        [JsonProperty("OptionsVersion")]
+        public Version OptionsVersion { get; set; }
+    }
+
+    public partial class Content
+    {
+        [JsonProperty("BundleContent")]
+        public string BundleContent { get; set; }
+
+        [JsonProperty("BundleName")]
+        public string BundleName { get; set; }
+
+        [JsonProperty("Key")]
+        public string Key { get; set; }
+    }
+
+    public partial class Choices
+    {
+        [JsonProperty("default")]
+        public string Default { get; set; }
+
+        [JsonProperty("v02")]
+        public string V02 { get; set; }
+
+        [JsonProperty("v02a")]
+        public string V02a { get; set; }
+
+        [JsonProperty("v02b")]
+        public string V02b { get; set; }
+
+        [JsonProperty("v02c")]
+        public string V02c { get; set; }
+    }
+
+    public partial class List
+    {
+        [JsonProperty("Choices")]
+        public Choices Choices { get; set; }
+
+        [JsonProperty("enabled")]
+        public bool Enabled { get; set; }
+
+        [JsonProperty("hoverTextDictionary")]
+        public Choices HoverTextDictionary { get; set; }
+
+        [JsonProperty("identifier")]
+        public string Identifier { get; set; }
+
+        [JsonProperty("LabelText")]
+        public string LabelText { get; set; }
+
+        [JsonProperty("Selection")]
+        public string Selection { get; set; }
+
+        [JsonProperty("SelectionIndex")]
+        public long? SelectionIndex { get; set; }
+
+        [JsonProperty("type")]
+        public long? Type { get; set; }
+    }
+
+    public partial class ModManifest
+    {
+        [JsonProperty("Author")]
+        public string Author { get; set; }
+
+        [JsonProperty("Dependencies")]
+        public Dependency[] Dependencies { get; set; }
+
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+
+        [JsonProperty("EntryDll")]
+        public string EntryDll { get; set; }
+
+        [JsonProperty("MinimumApiVersion")]
+        public object MinimumApiVersion { get; set; }
+
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("UniqueID")]
+        public string UniqueID { get; set; }
+
+        [JsonProperty("UpdateKeys")]
+        public object UpdateKeys { get; set; }
+
+        [JsonProperty("Version")]
+        public Version Version { get; set; }
+    }
+
+    public partial class Version
+    {
+        [JsonProperty("Build")]
+        public object Build { get; set; }
+
+        [JsonProperty("MajorVersion")]
+        public long MajorVersion { get; set; }
+
+        [JsonProperty("MinorVersion")]
+        public long MinorVersion { get; set; }
+
+        [JsonProperty("PatchVersion")]
+        public long PatchVersion { get; set; }
+    }
+
+    public partial class Dependency
+    {
+        [JsonProperty("IsRequired")]
+        public bool IsRequired { get; set; }
+
+        [JsonProperty("MinimumVersion")]
+        public object MinimumVersion { get; set; }
+
+        [JsonProperty("UniqueID")]
+        public string UniqueID { get; set; }
+    }
+
+    public partial class ParsBundles
+    {
+        public static ParsBundles[] FromJson(string json) => JsonConvert.DeserializeObject<ParsBundles[]>(json, Converter.Settings);
+    }
+
+    public partial class CCBOConfigParser
+    {
+        public static CCBOConfigParser FromJson(string json) => JsonConvert.DeserializeObject<CCBOConfigParser>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this ParsBundles[] self) => JsonConvert.SerializeObject(self, Converter.Settings);
+        public static string ToJson(this CCBOConfigParser self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    public class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+        };
+    }
+}
