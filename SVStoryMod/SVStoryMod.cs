52924113f330044d299236184727f0c027ee4fa5
Chaos234 <info@deltanet-production.de>
2018-05-16 20:20:05 +0200

    Some changes to the code, incl. finishing the de.json translation.

---
+++ SVStoryMod/SVStoryMod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using StardewConfigFramework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace SVStoryMod
+{
+    public class SVStoryMod : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+
+        }
+    }
+}
